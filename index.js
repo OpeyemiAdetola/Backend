3b2c545b0ef51f9c46860e602c73be3201eeb67d
Opeyemi <Ope.adet@gmail.com>
2022-06-16 21:24:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,101 @@
-const express = require('express');
-const app = express();
-const config = require('./config');
-const Food = require('./models/Food');
-const Offer = require('./models/Offer');
-const Beverage = require('./models/Beverage');
+const express = require('express'); // to import express
+const app = express(); // to create an app
+const mysql = require('mysql'); // to import mysql
+const myModule = require('./myModule');
+const sequelize = require('./config');
 const cors = require('cors');
+
 app.use(cors());
 
-
-config.authenticate().then(function () {
-    console.log('Database is connected');
-}).catch(function (err) {
-    console.log(err);
-});
-
-
-//FOOD  ROUTE
-
-
-app.get('/', function(req, res){
-    
-    Food.create(req.body).then(function (result) {
-        console.log(req.body)});
-});
-   
+const Food = require('./Models/food');
+const Beverage = require('./Models/beverage');
+const Offer = require('./Models/offer');
+const Foodtype = require('./Models/foodtype');
 
 
 
-app.post('/', function (req, res) {
-    Food.create(req.body).then(function (result) {
-        console.log(req.body);
-        res.redirect('/');
-    }).catch(function (err) {
-        res.send(err);
-    });
+app.use(express.urlencoded({ extended: false }));// for res.body to work
+app.use(express.json()); // required if data from client is sent in a JSON format
+
+//create a DB connection
+let conn = mysql.createConnection({
+    host: 'qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'jjg2a9gl405m55zf5',
+    password: 'tw6nal2atinb30jp',
+    database: 'w47ya69v92bjuqie'
 });
 
 
 
-app.delete('/:food_id', function (req, res) {
-    let taskId = req.params.task_id;
 
-    //Find the task by ID
-    Food.findByPk(foodId).then(function (result) {
 
-        if (result) {
-            //Delete task from database
-            result.destroy().then(function () {
-                res.redirect('/');
-            }).catch(function (err) {
-                res.status(500).send(err);
-            });
+
+//GET Routes
+//FOOD
+app.get('/', function (req, res) {
+    let sql = `SELECT * FROM foods`;
+    conn.query(sql, function (err, result) {
+        if (err) {
+            res.send(err);
         }
         else {
-            res.status(404).send('Task not found');
+            res.send(result);
         }
 
-    }).catch(function (err) {
-        res.status(500).send(err);
     });
 });
+
+
+
+
+
+
+
+//POST routes
+app.post('/', function (req, res) {
+    let foodData = req.body;
+    let sql = `INSERT INTO foods (id,food_name, description, price, foodtype_id) VALUES ('${foodData.id}','${foodData.food_name}', '${foodData.description}', ${foodData.price},'${foodData.foodtype_id}')`;
+    conn.query(sql, function (err, result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.redirect('/');
+        }
+
+    });
+});
+
+
+//DELETE ROUTES
+
+app.delete('/', function (req, res) {
+    let foodData = req.body;
+    let sql = `DELETE FROM foods (food_name, description, price, group_id) VALUES ('${foodData.food_name}', '${foodData.description}', ${foodData.price},'${foodData.group_id}')`;
+    conn.query(sql, function (err, result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.redirect('/');
+        }
+
+    });
+});
+
+
+
 
 
 function custom_middleware(req, res, next) {
     console.log('welcome to the home page')
     next();
-}
+};
 
-app.listen(5000, function () {
-    console.log('Server running on port 5000...');
-});
+
+
+
+app.listen(4000, function () {
+    console.log('Server running on port 4000...');
+}); // to create a web server
+
